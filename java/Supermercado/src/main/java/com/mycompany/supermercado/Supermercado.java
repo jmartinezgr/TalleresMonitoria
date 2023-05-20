8f1f2658b5354aae1f9c46f61f88c5bca7e59648
Juan Jose Martinez <josemargri3@gmail.com>
2023-05-20 17:57:13 -0500

    Added some missed files

---
+++ java/Supermercado/src/main/java/com/mycompany/supermercado/Supermercado.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermercado;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 57314
+ */
+public class Supermercado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ prueba/app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+from datetime import date
+
+print(date.today())
